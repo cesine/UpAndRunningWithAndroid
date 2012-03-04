4e596a5401013ecb9049e695c31f7cc4dc02dd5e
Theresa Deering <trisapeace@gmail.com>
2012-03-04 15:50:02 -0500

    See previous message

---
+++ android/src/com/androidmontreal/commander/RoogleCommander.java
@@ -0,0 +1,29 @@
+package com.androidmontreal.arduino.commander;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RoogleCommander extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // Send "My Status" to MyRobots.com
+        Intent svc = new Intent(this, MyRobotsSender.class);
+        svc.putExtra(MyRobotsSender.STATUS, "My Status");
+        startService(svc);
+        
+        // Get the latest status from MyRobots.com
+        Intent svcReceiver = new Intent(this, MyRobotsReceiver.class);
+        startService(svcReceiver);
+        
+        // Display "Send status"
+        TextView tv = new TextView(this);
+        tv.setText("Sent status");
+        setContentView(tv);
+    }
+}
