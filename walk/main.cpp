128f7bc85f912d3489f61ebee6785e19cc6b5db6
Martin Fixman <martinfixman@gmail.com>
2012-03-03 19:48:49 -0500

    Generic commit message.

--- walk/main.cpp
+++ walk/main.cpp
@@ -33,49 +33,49 @@
 
 void change_current_dir()
 {
-	char exepath[1024] = {0};
-	if(readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
-		chdir(dirname(exepath));
+		char exepath[1024] = {0};
+		if(readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
+				chdir(dirname(exepath));
 }
 
 int main(void)
 {
-	printf( "\n===== Action script Tutorial for DARwIn =====\n\n");
+		printf( "\n===== Action script Tutorial for DARwIn =====\n\n");
 
-	change_current_dir();
+		change_current_dir();
 
-	LinuxCM730 linux_cm730(U2D_DEV_NAME);
-	CM730 cm730(&linux_cm730);
-	if(MotionManager::GetInstance()->Initialize(&cm730) == false)
-	{
-		printf("Fail to initialize Motion Manager!\n");
-		return 0;
-	}
+		LinuxCM730 linux_cm730(U2D_DEV_NAME);
+		CM730 cm730(&linux_cm730);
+		if(MotionManager::GetInstance()->Initialize(&cm730) == false)
+		{
+				printf("Fail to initialize Motion Manager!\n");
+				return 0;
+		}
 
 
-	int n = 0;
-	int param[JointData::NUMBER_OF_JOINTS * 5];
-	int wGoalPosition, wStartPosition, wDistance;
+		int n = 0;
+		int param[JointData::NUMBER_OF_JOINTS * 5];
+		int wGoalPosition, wStartPosition, wDistance;
 
-	for(int id=JointData::ID_R_SHOULDER_PITCH; id<JointData::NUMBER_OF_JOINTS; id++)
-	{
-		wStartPosition = MotionStatus::m_CurrentJoints.GetValue(id);
-		wGoalPosition = Walking::GetInstance()->m_Joint.GetValue(id);
-		if( wStartPosition > wGoalPosition )
-			wDistance = wStartPosition - wGoalPosition;
-		else
-			wDistance = wGoalPosition - wStartPosition;
+		for(int id=JointData::ID_R_SHOULDER_PITCH; id<JointData::NUMBER_OF_JOINTS; id++)
+		{
+				wStartPosition = MotionStatus::m_CurrentJoints.GetValue(id);
+				wGoalPosition = Walking::GetInstance()->m_Joint.GetValue(id);
+				if( wStartPosition > wGoalPosition )
+						wDistance = wStartPosition - wGoalPosition;
+				else
+						wDistance = wGoalPosition - wStartPosition;
 
-		wDistance >>= 2;
-		if( wDistance < 8 )
-			wDistance = 8;
+				wDistance >>= 2;
+				if( wDistance < 8 )
+						wDistance = 8;
 
-		param[n++] = id;
-		param[n++] = CM730::GetLowByte(wGoalPosition);
-		param[n++] = CM730::GetHighByte(wGoalPosition);
-		param[n++] = CM730::GetLowByte(wDistance);
-		param[n++] = CM730::GetHighByte(wDistance);
-	}
-	cm730.SyncWrite(P_GOAL_POSITION_L, 5, JointData::NUMBER_OF_JOINTS - 1, param);
+				param[n++] = id;
+				param[n++] = CM730::GetLowByte(wGoalPosition);
+				param[n++] = CM730::GetHighByte(wGoalPosition);
+				param[n++] = CM730::GetLowByte(wDistance);
+				param[n++] = CM730::GetHighByte(wDistance);
+		}
+		cm730.SyncWrite(MX28::P_GOAL_POSITION_L, 5, JointData::NUMBER_OF_JOINTS - 1, param);
 
 }
