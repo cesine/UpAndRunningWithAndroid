a6122cee865638cdb5a7dbe0f87ff203269d9761
cesine <cesine@yahoo.com>
2013-02-26 16:10:23 -0500

    reverting make it talk back to the original so you can see the progression

--- Tutorial/src/com/androidmontreal/gesturevoicecommander/practice/MakeItTalk.java
+++ Tutorial/src/com/androidmontreal/gesturevoicecommander/practice/MakeItTalk.java
@@ -1,12 +1,9 @@
 package com.androidmontreal.gesturevoicecommander.practice;
 
-import java.util.ArrayList;
 import java.util.Locale;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
@@ -14,19 +11,8 @@ import android.widget.Toast;
 
 import com.androidmontreal.gesturevoicecommander.R;
 
-/**
- * 
- * Building on what we saw in MakeItTalk, now lets make it Listen. Here is some
- * super simple code that uses the VoiceRecognition Intent to recognize what the
- * user says, and then uses Text To Speech to tell the user what it might have
- * heard.
- * 
- * @author cesine
- * 
- */
 public class MakeItTalk extends Activity implements OnInitListener {
-  private static final String TAG = "MakeItListen";
-  private static final int RETURN_FROM_VOICE_RECOGNITION_REQUEST_CODE = 341;
+  private static final String TAG = "MakeItTalk";
   /** Talk to the user */
   private TextToSpeech mTts;
 
@@ -38,60 +24,8 @@ public class MakeItTalk extends Activity implements OnInitListener {
 
   }
 
-  protected void promptTheUserToTalk() {
-    mTts.speak(getString(R.string.im_listening), TextToSpeech.QUEUE_ADD, null);
-  }
-
-  /**
-   * Fire an intent to start the voice recognition activity.
-   */
-  private void startVoiceRecognitionActivity() {
-    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-    intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-        getString(R.string.im_listening));
-    startActivityForResult(intent, RETURN_FROM_VOICE_RECOGNITION_REQUEST_CODE);
-  }
-
-  /**
-   * Handle the results from the voice recognition activity.
-   */
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == RETURN_FROM_VOICE_RECOGNITION_REQUEST_CODE
-        && resultCode == RESULT_OK) {
-      /*
-       * Populate the wordsList with the String values the recognition engine
-       * thought it heard, and then Toast them to the user and say them out
-       * loud.
-       */
-      ArrayList<String> matches = data
-          .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-      for (int iMightHaveHeardThis = 0; iMightHaveHeardThis < matches.size(); iMightHaveHeardThis++) {
-
-        /* Build a carrierPhrase if you want it to make some sense */
-        String carrierPhrase = getString(R.string.i_might_have_heard);
-        if (iMightHaveHeardThis > 0) {
-          carrierPhrase = getString(R.string.or_maybe);
-        }
-        carrierPhrase += " " + matches.get(iMightHaveHeardThis) + ".";
-
-        Toast.makeText(this, carrierPhrase, Toast.LENGTH_LONG).show();
-        mTts.speak(carrierPhrase, TextToSpeech.QUEUE_ADD, null);
-
-        /*
-         * Don't go on forever, it there are too many potential matches don't
-         * say them all
-         */
-        if (iMightHaveHeardThis == 2 && matches.size() > 2) {
-          mTts.speak(getString(R.string.there_were_others),
-              TextToSpeech.QUEUE_ADD, null);
-          break;
-        }
-      }
-    }
-    super.onActivityResult(requestCode, resultCode, data);
+  protected void sayFirstWords() {
+    mTts.speak(getString(R.string.my_first_words), TextToSpeech.QUEUE_ADD, null);
   }
 
   @Override
@@ -119,12 +53,12 @@ public class MakeItTalk extends Activity implements OnInitListener {
             Toast.LENGTH_LONG).show();
       } else {
         // everything is working.
-        promptTheUserToTalk();
-        startVoiceRecognitionActivity();
+        sayFirstWords();
       }
     } else {
       Toast.makeText(
           this,
+
           "Sorry, I can't talk to you because "
               + "I could not initialize TextToSpeech.", Toast.LENGTH_LONG)
           .show();
